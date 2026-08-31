bb92c84a442c932ee65ca11b8e18cb8d6beb89af
srivarshithsai <srivarshithsaiwummadisetty@gmail.com>
2026-08-31 16:01:05 +0530

    Update arithmetic operators.py

--- arithmetic operators.py
+++ arithmetic operators.py
@@ -1,6 +1,5 @@
 a=7
 b=4
-#print("sum of a and b is:",a+b)#addition operator
 print("addition of a and b is:",a+b)
 print("substraction of a and b is:",a-b)
 print("Multiplication of a and b is:",a*b)
