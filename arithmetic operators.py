536286897d954339046d2eaee1149126d0030ed5
srivarshithsai <srivarshithsaiwummadisetty@gmail.com>
2026-08-31 16:03:25 +0530

    Delete arithmetic operators.py

--- arithmetic operators.py
+++
@@ -1,9 +0,0 @@
-a=7
-b=4
-print("addition of a and b is:",a+b)
-print("substraction of a and b is:",a-b)
-print("Multiplication of a and b is:",a*b)
-print("Division of a and b is:",a/b)
-print("Modulus of a and b is:",a%b)
-print("floor division of a and b is:",a//b)
-print("Exponentiation of a and b is:",a**b)
