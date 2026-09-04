5a97b13cdc3e24cbc0af307416af9a15233d8a6a
srivarshithsai <srivarshithsaiwummadisetty@gmail.com>
2026-09-04 13:20:55 +0530

    Comparison operators

---
+++ comparison operators.py
@@ -0,0 +1,15 @@
+a=3
+b=9
+result = a > b
+print("result of",a, ">",b,"is :",result)
+result = a < b
+print("result of",a, "<",b, "is :",result)
+result = a == b
+print("result of",a, "==",b, "is :",result)
+result = a != b
+print("result of",a, "!=",b, "is :",result)
+result = a <= b 
+print("result of",a, "<=",b, "is :",result)
+result = a >= b
+print("result of",a, ">=",b, "is :",result)
+
