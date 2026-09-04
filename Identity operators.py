f9e745b3a154ca875592945756233d4055113989
srivarshithsai <srivarshithsaiwummadisetty@gmail.com>
2026-09-04 13:23:46 +0530

    Identity operators

---
+++ Identity operators.py
@@ -0,0 +1,15 @@
+# Python Iddentity operators
+#numeric
+a=[24,25,26]
+b=a
+print("result of a is b if both are the same objects:",a is b)
+a=[24,25,26]
+b=[24,25,26]
+print("result of a is b if both are not the same objects:",a is b)
+#sequnence
+a=["mango,apple,bannana"]
+b=a
+print("result of a is b if both are the same objects:",a is b)
+a=["mango,apple,bannana"]
+b=["mango,apple,bannana"]
+print("result of a is b if both are not the same objects:",a is b)
