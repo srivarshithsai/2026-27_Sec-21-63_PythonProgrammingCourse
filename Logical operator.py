8ea5306816baa4356d981ddddac1afb54d9e28a7
srivarshithsai <srivarshithsaiwummadisetty@gmail.com>
2026-09-04 13:44:27 +0530

    Logical operator

---
+++ Logical operator.py
@@ -0,0 +1,13 @@
+#Logical operator
+#AND operator
+a=3
+result=a>10 and a<4
+print("result of",a,">10 and ",a,"<4 is",result)
+#OR operator
+a=4
+result=a>2 or a<6
+print("result of",a,">2 or ",a,"<6 is",result)
+#NOT operator
+a=8
+result=not(a>9 and a<10)
+print("result of ",a,">9 not ",a,"<10 is",result)
