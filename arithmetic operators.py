9e714dce9696dd2b75e6b1385cb83f5ca4942f78
srivarshithsai <srivarshithsaiwummadisetty@gmail.com>
2026-09-04 13:18:21 +0530

    Arithmetic operators

---
+++ arithmetic operators.py
@@ -0,0 +1,10 @@
+a=7
+b=4
+#print("sum of a and b is:",a+b)#addition operator
+print("addition of a and b is:",a+b)
+print("substraction of a and b is:",a-b)
+print("Multiplication of a and b is:",a*b)
+print("Division of a and b is:",a/b)
+print("Modulus of a and b is:",a%b)
+print("floor division of a and b is:",a//b)
+print("Exponentiation of a and b is:",a**b)
