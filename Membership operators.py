084fbf02a6ee4526d6bc4aa258246676751925cf
srivarshithsai <srivarshithsaiwummadisetty@gmail.com>
2026-09-04 14:21:44 +0530

    Membership operators

---
+++ Membership operators.py
@@ -0,0 +1,13 @@
+#Membership operators
+#IN operator
+A='a' in 'apple'
+print("result of a in apple," "is",A) 
+A='x' in 'apple'
+print("result of x in apple," "is",A)
+#NOT IN operator
+B={4,3,5,6}
+result=4 not in B
+print("result of 4 is not in",B,"is",result)
+B={2,3,4,5}
+result=5 in B
+print("result of 5 in",B,"is",result)
