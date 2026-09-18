628267b948702ad564a475ad9f3a513dcd002978
srivarshithsai <srivarshithsaiwummadisetty@gmail.com>
2026-09-18 13:58:25 +0530

    Lab task_10

---
+++ lab task10.py
@@ -0,0 +1,14 @@
+a=int(input("enter the number 1:"))
+b=int(input("enter the number 2:"))
+result = a > b
+print("result of",a, ">",b,"is :",result)
+result = a < b
+print("result of",a, "<",b, "is :",result)
+result = a == b
+print("result of",a, "==",b, "is :",result)
+result = a != b
+print("result of",a, "!=",b, "is :",result)
+result = a <= b 
+print("result of",a, "<=",b, "is :",result)
+result = a >= b
+print("result of",a, ">=",b, "is :",result)
