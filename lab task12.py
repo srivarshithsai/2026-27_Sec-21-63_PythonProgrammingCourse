a3810dbca1aa3ab97cc8815ad30c123646173390
srivarshithsai <srivarshithsaiwummadisetty@gmail.com>
2026-09-18 13:59:51 +0530

    Lab task_12

---
+++ lab task12.py
@@ -0,0 +1,7 @@
+# Python Identity operators
+a=[7,8,9]
+b=a
+print("result of a is b if both are the same objects:",a is b)
+a=[4,5,6]
+b=[4,5,6]
+print("result of a is b if both are not the same objects:",a is b)
