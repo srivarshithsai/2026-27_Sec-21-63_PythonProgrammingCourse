b685ed42714d83f8771549f49882b97532b96e01
srivarshithsai <srivarshithsaiwummadisetty@gmail.com>
2026-09-18 14:00:13 +0530

    Lab task_13

---
+++ lab task13.py
@@ -0,0 +1,7 @@
+#write a python to demonstrate membership opertors(in,not in)
+B={2,3,4,5}
+result=4 not in B
+print("result of 4 is not in",B,"is",result)
+B={2,3,4,5}
+result=5 in B
+print("result of 5 in",B,"is",result)
