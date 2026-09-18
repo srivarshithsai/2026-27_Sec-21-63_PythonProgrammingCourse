48d6af06caba122e4ff758591b0041061ad9fb29
srivarshithsai <srivarshithsaiwummadisetty@gmail.com>
2026-09-18 13:55:27 +0530

    Lab task_6

---
+++ lab task6.py
@@ -0,0 +1,5 @@
+Principal=float(input("enter the principal amount:"))
+Rate=float(input("enter the rate of interset in %:"))
+Time=float(input("enter the time in years:"))
+simpleinterest =Principal*Rate*Time/100
+print(simpleinterest)   
